bf4f581b9b23d99c1562e92b192c5f642af82ba1
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-05-31 17:53:34 -0500

    Update Tetris.java

--- Homeworks/Tetris.java
+++ Homeworks/Tetris.java
@@ -43,6 +43,7 @@ class getTetris extends Canvas {
 		initgr();
 		float square = (float) (minMaxXY / 30);
 		float xA, yA, xB, yB, xC, yC, xD, yD, xE, yE, xF, yF, xG, yG;
+		float xH, yH, xI, yI, xJ, yJ, xK, yK, xL, yL, xM, yM;
 		xA = (float) xCenter - square * 5;
 		yA = (float) yCenter - square * 10;
 		xB = (float) xCenter + square * 6;
@@ -57,13 +58,40 @@ class getTetris extends Canvas {
 		yF = yB + (float) 1.5 * square;
 		xG = (float) xCenter - square;
 		yG = (float) yCenter - square * 6;
+		xI = (float) xCenter + square * 6;
+		yI = (float) yCenter + square;
+		xH = xI;
+		yH = yI - square * 2;
+		xJ = xH;
+		yJ = yI + 2 * square;
+		xK = (float) xCenter + square * 6;
+		yK = (float) yCenter + square * (float)8.5;
+		xL = (float) (xK + 0.9 * square);
+		yL = (float) (yK + 1.1 * square);
+		xM = (float) (xC + 0.9 * square);
+		yM = (float) (yC + 1.4 * square);
 		g.drawRect(iX(xA), iX(yA), iX(square * 10), iX(square * 20));
 		g.drawRect(iX(xB), iX(yB), iX(square * 5), iX(square * 3));
+		g.drawRect(iX(xK), iX(yK), iX(square * 4), iX(square * (float)1.5));
+		g.setColor(Color.BLUE);
 		g.drawRect(iX(xC), iX(yC), iX(square * 5), iX(square * 2));
 		drawWedge(g, iX(xD), iX(yD), (int) (square));
 		drawL(g, iX(xE), iX(yE), (int) (square));
 		drawReverseL(g, iX(xF), iX(yF), (int) (square));
 		drawSquare(g, iX(xG), iX(yG), (int) (square));
+		
+		Font f = new Font("Dialog", Font.BOLD, (int)square);
+		g.setFont(f);
+		g.drawString("Level:      1", iX(xH), iX(yH));
+		g.drawString("Lines:      0", iX(xI), iX(yI));
+		g.drawString("QUIT", iX(xL), iX(yL));
+		f = new Font("Dialog", Font.BOLD, (int)(square*0.999));
+		g.setFont(f);
+		g.drawString("Score:      0", iX(xJ), iX(yJ));
+		f = new Font("Dialog", Font.BOLD, (int)(square));
+		g.setColor(Color.BLUE);
+		g.drawString("PAUSE", iX(xM), iX(yM));
+
 	}
 
 	void drawWedge(Graphics g, int x, int y, int square) {
