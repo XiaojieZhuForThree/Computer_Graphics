4907f2f8bb72173f69b712406be7e5d034b9c0b6
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-06-05 11:51:00 -0500

    Update Tetris.java

--- Homeworks/Tetris.java
+++ Homeworks/Tetris.java
@@ -21,7 +21,11 @@ public class Tetris extends Frame {
 
 class getTetris extends Canvas {
 	int maxX, maxY, minMaxXY, xCenter, yCenter;
-
+	boolean show = false;
+	float square;
+	float xA, yA, xB, yB, xC, yC, xD, yD, xE, yE, xF, yF, xG, yG;
+	float xH, yH, xI, yI, xJ, yJ, xK, yK, xL, yL, xM, yM;
+	int mouse1x, mouse1y, mouse2x, mouse2y;
 	void initgr() {
 		Dimension d = getSize();
 		maxX = d.width - 1;
@@ -38,48 +42,88 @@ class getTetris extends Canvas {
 	int iY(float y) {
 		return maxY - Math.round(y);
 	}
+	
+	getTetris() {
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent evt) {
+				mouse1x = evt.getX();
+				mouse1y = evt.getY();
+				if ((mouse1x < iX(xK + square * 4)) && (mouse1x > iX(xK)) && (mouse1y < iY(yK - square * (float) 1.5))
+						&& (mouse1y > iY(yK))) {
+					System.exit(0);
+				}
+			}
+		});
 
+		addMouseMotionListener(new MouseAdapter() {
+			public void mouseMoved(MouseEvent evtM) {
+				mouse2x = evtM.getX();
+				mouse2y = evtM.getY();
+				if ((mouse2x < iX(xA + square * 10)) && (mouse2x > iX(xA)) && (mouse2y < iY(yA - square * 20)) && (mouse2y > iY(yA))) {
+					show = true;
+					repaint();
+				} else {
+					show = false;
+					repaint();
+				}
+			}
+		});
+	}
 	public void paint(Graphics g) {
+		square = (float) (minMaxXY / 30);
 		initgr();
-		float square = (float) (minMaxXY / 30);
-		float xA, yA, xB, yB, xC, yC, xD, yD, xE, yE, xF, yF, xG, yG;
-		float xH, yH, xI, yI, xJ, yJ, xK, yK, xL, yL, xM, yM;
-		
-		xA = (float) xCenter - square * 5; yA = (float) yCenter + square * 10;
-		xB = (float) xCenter + square * 6; yB = yA;
-		xC = (float) ((float) xCenter - square * 2.5); yC = (float) yCenter + square;
-		xD = (float) xCenter + square; yD = (float) yCenter - square * 9;
-		xE = (float) xCenter + square * 3; yE = (float) yCenter - square * 9;
-		xF = xB + square; yF = yB - (float) 1.5 * square;
-		xG = (float) xCenter - square; yG = (float) yCenter + square * 6;
-		xI = (float) xCenter + square * 6; yI = (float) yCenter - square;
-		xH = xI; yH = yI + square * 2;
-		xJ = xH; yJ = yI - 2 * square;
-		xK = (float) xCenter + square * 6; yK = (float) yCenter - square * (float)8.5;
-		xL = (float) (xK + 0.9 * square); yL = (float) (yK - 1.1 * square);
-		xM = (float) (xC + 0.9 * square); yM = (float) (yC - 1.4 * square);
-		
-		g.drawRect(iX(xA), iY(yA), (int)(square * 10), (int)(square * 20));
-		g.drawRect(iX(xB), iY(yB), (int)(square * 5), (int)(square * 3));
-		g.drawRect(iX(xK), iY(yK), (int)(square * 4), (int)(square * (float)1.5));
-		g.setColor(Color.BLUE);
-		g.drawRect(iX(xC), iY(yC), (int)(square * 5), (int)(square * 2));
+		xA = (float) xCenter - square * 5;
+		yA = (float) yCenter + square * 10;
+		xB = (float) xCenter + square * 6;
+		yB = yA;
+		xC = (float) ((float) xCenter - square * 2.5);
+		yC = (float) yCenter + square;
+		xD = (float) xCenter + square;
+		yD = (float) yCenter - square * 9;
+		xE = (float) xCenter + square * 3;
+		yE = (float) yCenter - square * 9;
+		xF = xB + square;
+		yF = yB - (float) 1.5 * square;
+		xG = (float) xCenter - square;
+		yG = (float) yCenter + square * 6;
+		xI = (float) xCenter + square * 6;
+		yI = (float) yCenter - square;
+		xH = xI;
+		yH = yI + square * 2;
+		xJ = xH;
+		yJ = yI - 2 * square;
+		xK = (float) xCenter + square * 6;
+		yK = (float) yCenter - square * (float) 8.5;
+		xL = (float) (xK + 0.9 * square);
+		yL = (float) (yK - 1.1 * square);
+		xM = (float) (xC + 0.9 * square);
+		yM = (float) (yC - 1.4 * square);
+
+		g.drawRect(iX(xA), iY(yA), (int) (square * 10), (int) (square * 20));
+		g.drawRect(iX(xB), iY(yB), (int) (square * 5), (int) (square * 3));
+		g.drawRect(iX(xK), iY(yK), (int) (square * 4), (int) (square * (float) 1.5));
+
+
 		drawWedge(g, iX(xD), iY(yD), (int) (square));
 		drawL(g, iX(xE), iY(yE), (int) (square));
 		drawReverseL(g, iX(xF), iY(yF), (int) (square));
-		drawSquare(g, iX(xG), iY(yG), (int) (square));
-		
-		Font f = new Font("Dialog", Font.BOLD, (int)square);
+		drawCube(g, iX(xG), iY(yG), (int) (square));
+
+		Font f = new Font("Dialog", Font.BOLD, (int) square);
 		g.setFont(f);
 		g.drawString("Level:      1", iX(xH), iY(yH));
 		g.drawString("Lines:      0", iX(xI), iY(yI));
 		g.drawString("QUIT", iX(xL), iY(yL));
-		f = new Font("Dialog", Font.BOLD, (int)(square*0.999));
+		f = new Font("Dialog", Font.BOLD, (int) (square * 0.999));
 		g.setFont(f);
 		g.drawString("Score:      0", iX(xJ), iY(yJ));
-		f = new Font("Dialog", Font.BOLD, (int)(square));
-		g.setColor(Color.BLUE);
-		g.drawString("PAUSE", iX(xM), iY(yM));
+		if (show) {
+			g.setColor(Color.BLUE);
+			g.drawRect(iX(xC), iY(yC), (int) (square * 5), (int) (square * 2));
+			f = new Font("Dialog", Font.BOLD, (int) (square));
+			g.setColor(Color.BLUE);
+			g.drawString("PAUSE", iX(xM), iY(yM));			
+		}
 
 	}
 
@@ -134,7 +178,7 @@ class getTetris extends Canvas {
 		}
 	}
 
-	void drawSquare(Graphics g, int x, int y, int square) {
+	void drawCube(Graphics g, int x, int y, int square) {
 		int[][] vertices = new int[4][2];
 		vertices[0] = new int[] { x, y };
 		vertices[1] = new int[] { x + square, y };
@@ -150,4 +194,5 @@ class getTetris extends Canvas {
 
 		}
 	}
+
 }
