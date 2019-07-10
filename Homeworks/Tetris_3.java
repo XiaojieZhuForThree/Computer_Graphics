804a5821242556bbb9db45bb66db7079f15829fd
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-07-10 18:34:32 -0500

    Update Tetris_3.java

--- Homeworks/Tetris_3.java
+++ Homeworks/Tetris_3.java
@@ -26,23 +26,41 @@ import javax.swing.Timer;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-//class ScoringFactor {
-//	JSlider bar;
-//	JLabel label;
-//
-//	ScoringFactor() {
-//		bar = new JSlider(JSlider.HORIZONTAL, 1, 10, 1);
-//		bar.setMajorTickSpacing(1);
-//		bar.setPaintTicks(true);
-//	}
-//
-//	class event implements ChangeListener {
-//		public void stateChanged(ChangeEvent e) {
-//			int value = bar.getValue();
-//			label.setText("Scoring Factor: " + value);
-//		}
-//	}
-//}
+class Square {
+	int X, Y;
+	Color color;
+
+	Square(int x, int y, Color cl) {
+		X = x;
+		Y = y;
+		color = cl;
+	}
+}
+
+class Point2D {
+	float x, y;
+
+	Point2D(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
+
+class Tools2D {
+	static float area2(Point2D a, Point2D b, Point2D c) {
+		return (a.x - c.x) * (b.y - c.y) - (a.y - c.y) * (b.x - c.x);
+	}
+
+	static float distance2(Point2D p, Point2D q) {
+		float dx = p.x - q.x, dy = p.y - q.y;
+		return dx * dx + dy * dy;
+	}
+
+	static boolean insideTriangle(Point2D a, Point2D b, Point2D c, Point2D p) {
+		return area2(a, b, p) >= 0 && area2(b, c, p) >= 0 && area2(c, a, p) >= 0;
+	}
+}
 
 public class Tetris_3 extends Frame {
 	// global variables
@@ -51,6 +69,58 @@ public class Tetris_3 extends Frame {
 
 	JSlider levelClimber;
 	JLabel lcLabel;
+
+	JSlider speedFactor;
+	JLabel spLabel;
+	Panel panel;
+
+	public void setPanel() {
+
+		scoringFactor = new JSlider(JSlider.HORIZONTAL, 1, 10, 1);
+		scoringFactor.setMajorTickSpacing(1);
+		scoringFactor.setPaintTicks(true);
+		sfLabel = new JLabel("Scoring Factor: 1");
+
+		scoringFactor.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				sfLabel.setText(String.valueOf("Scoring Factor: " + scoringFactor.getValue()));
+			}
+		});
+
+		levelClimber = new JSlider(JSlider.HORIZONTAL, 20, 50, 20);
+		levelClimber.setMajorTickSpacing(5);
+		levelClimber.setPaintTicks(true);
+		lcLabel = new JLabel("Lines Removed Required for level up: 20");
+
+		levelClimber.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				lcLabel.setText(String.valueOf("Lines Removed Required for level up: " + levelClimber.getValue()));
+			}
+		});
+
+		speedFactor = new JSlider(JSlider.HORIZONTAL, 1, 10, 1);
+		speedFactor.setMajorTickSpacing(1);
+		speedFactor.setPaintTicks(true);
+		spLabel = new JLabel("Speed Factor: 0.1");
+
+		speedFactor.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				spLabel.setText(String.valueOf("Speed Factor: " + ((float) speedFactor.getValue() / 10)));
+			}
+		});
+
+		panel = new Panel();
+		panel.add(sfLabel);
+		panel.add(scoringFactor);
+		panel.add(lcLabel);
+		panel.add(levelClimber);
+		panel.add(spLabel);
+		panel.add(speedFactor);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+	}
 
 	public static void main(String[] args) {
 		new Tetris_3();
@@ -66,12 +136,6 @@ public class Tetris_3 extends Frame {
 		});
 
 		add(new getTetris3(), BorderLayout.CENTER);
-		Panel panel = new Panel();
-		panel.add(sfLabel);
-		panel.add(scoringFactor);
-		panel.add(lcLabel);
-		panel.add(levelClimber);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		add(panel, BorderLayout.EAST);
 		setSize(800, 600);
 		setVisible(true);
@@ -96,8 +160,11 @@ public class Tetris_3 extends Frame {
 		int mouse1x, mouse1y, mouse2x, mouse2y;
 		List<int[]> prevDraws = new ArrayList<>();
 		Set<Square> prevs;
+		List<Point2D> checkpol;
 		// random number generator
 		Random rand = new Random();
+
+		Point2D[] pol;
 
 		// Obtain a number between [0 - 6].
 		int n1 = rand.nextInt(7);
@@ -106,10 +173,10 @@ public class Tetris_3 extends Frame {
 		int xMin, xMax, yMin, yMax;
 		int[][] nextCoord = new int[4][2];
 
-		int score = 0, M, N = 20;
+		int score = 0, M, N;
 		int level = 1, previousLevel = 1;
 		int removedRows = 0;
-		float S = 0.1F;
+		float S;
 
 		Timer timer = new Timer((int) delay, new ActionListener() {
 			@Override
@@ -119,6 +186,9 @@ public class Tetris_3 extends Frame {
 				boolean coincidence = newcheckCoincidence(nextCoord, prevs);
 				if (!coincidence && checkInside() && !show) {
 					count += 1;
+//					for (Point2D i : pol) {
+//						System.out.print("[" + i.x + ", " + i.y + "]");
+//					}
 					repaint();
 				} else if ((coincidence || !checkInside()) && !show) {
 //					checkValid(n1, xDraw, yDraw, rotate);
@@ -144,6 +214,8 @@ public class Tetris_3 extends Frame {
 			xCenter = maxX / 2;
 			yCenter = maxY / 2;
 			M = scoringFactor.getValue();
+			N = levelClimber.getValue();
+			S = ((float) speedFactor.getValue() / 10);
 		}
 
 		int iX(float x) {
@@ -154,17 +226,17 @@ public class Tetris_3 extends Frame {
 			return maxY - Math.round(y);
 		}
 
+		float fx(int x) {
+			return (float) x;
+		}
+
+		float fy(int y) {
+			return (float) (maxY - y);
+		}
+
 		getTetris3() {
 			// add the MouseListener to get the function of the QUIT function
-			scoringFactor = new JSlider(JSlider.HORIZONTAL, 1, 10, 1);
-			scoringFactor.setMajorTickSpacing(1);
-			scoringFactor.setPaintTicks(true);
-			sfLabel = new JLabel("Scoring Factor: 1");
-			levelClimber = new JSlider(JSlider.HORIZONTAL, 20, 50, 20);
-			levelClimber.setMajorTickSpacing(5);
-			levelClimber.setPaintTicks(true);
-			lcLabel = new JLabel("Lines Removed Required for level up: 20");
-
+			setPanel();
 			addMouseListener(new MouseAdapter() {
 				public void mousePressed(MouseEvent event) {
 					mouse1x = event.getX();
@@ -217,9 +289,13 @@ public class Tetris_3 extends Frame {
 				public void mouseMoved(MouseEvent event) {
 					mouse2x = event.getX();
 					mouse2y = event.getY();
+					Point2D p = new Point2D(mouse2x, mouse2y);
+
 					if ((mouse2x < iX(xA + square * 10)) && (mouse2x > iX(xA)) && (mouse2y < iY(yA - square * 20))
 							&& (mouse2y > iY(yA))) {
 						show = true;
+						if (!insidePolygon(p, pol)) {
+						}
 						repaint();
 					} else {
 						show = false;
@@ -228,6 +304,19 @@ public class Tetris_3 extends Frame {
 				}
 			});
 
+		}
+
+		boolean insidePolygon(Point2D p, Point2D[] pol) {
+			int n = pol.length, j = n - 1;
+			boolean b = false;
+			float x = p.x, y = p.y;
+			for (int i = 0; i < n; i++) {
+				if (pol[j].y <= y && y < pol[i].y && Tools2D.area2(pol[j], pol[i], p) > 0
+						|| pol[i].y <= y && y < pol[j].y && Tools2D.area2(pol[i], pol[j], p) > 0)
+					b = !b;
+				j = i;
+			}
+			return b;
 		}
 
 		public void paint(Graphics g) {
@@ -288,12 +377,12 @@ public class Tetris_3 extends Frame {
 			}
 			for (int i = 1; i <= removedRows; i++) {
 				score += level * M;
-				if (i % 5 == 0) {
+				if (i % N == 0) {
 					level++;
 				}
 			}
 			if (level > previousLevel) {
-				delay /= (1 + level);
+				delay /= (1 + level * S);
 				previousLevel = level;
 			}
 			timer.setDelay((int) delay);
@@ -390,6 +479,7 @@ public class Tetris_3 extends Frame {
 
 		// function to draw the current moving shape
 		void drawMoveShapes(Graphics g, int n1, float xDraw, float yDraw, int rotate) {
+
 			if (n1 == 0) {
 				drawYellowWedge(g, iX(xDraw - square), iY(yDraw), (int) square, rotate);
 			} else if (n1 == 1) {
@@ -404,6 +494,18 @@ public class Tetris_3 extends Frame {
 				drawOrangeHill(g, iX(xDraw - square), iY(yDraw), (int) square, rotate);
 			} else {
 				drawCyanBar(g, iX(xDraw - 2 * square), iY(yDraw + square), (int) square, rotate);
+			}
+			checkpol = new ArrayList<>();
+			for (int[] coord : nextCoord) {
+				int X = coord[0];
+				int Y = coord[1];
+				fillPoly(X, Y);
+			}
+			pol = new Point2D[checkpol.size()];
+			int i = 0;
+			for (Point2D j : checkpol) {
+				pol[i] = j;
+				i++;
 			}
 		}
 
@@ -465,6 +567,27 @@ public class Tetris_3 extends Frame {
 			}
 		}
 
+		void fillPoly(int X, int Y) {
+			int[][] points = new int[4][2];
+			points[0] = new int[] { X, Y };
+			points[1] = new int[] { X, Y - (int) square };
+			points[2] = new int[] { X + (int) square, Y };
+			points[3] = new int[] { X + (int) square, Y - (int) square };
+			for (int[] point : points) {
+				boolean removed = false;
+				for (int i = 0; i < checkpol.size(); i++) {
+					if (checkpol.get(i).x == fx(point[0]) && checkpol.get(i).y == fy(point[1])) {
+						checkpol.remove(i);
+						removed = true;
+					}
+				}
+				if (!removed) {
+					checkpol.add(new Point2D(fx(point[0]), fy(point[1])));
+				}
+			}
+
+		}
+
 		// functions to draw all the shapes
 		void drawYellowWedge(Graphics g, int x, int y, int square, int rotate) {
 			nextYellowWedge(x, y, square, rotate);
@@ -483,6 +606,7 @@ public class Tetris_3 extends Frame {
 			for (int[] coord : nextCoord) {
 				int X = coord[0];
 				int Y = coord[1];
+
 				g.setColor(Color.MAGENTA);
 				g.fillRect(X, Y, square, square);
 				g.setColor(Color.black);
@@ -519,6 +643,7 @@ public class Tetris_3 extends Frame {
 			for (int[] coord : nextCoord) {
 				int X = coord[0];
 				int Y = coord[1];
+
 				g.setColor(Color.GREEN);
 				g.fillRect(X, Y, square, square);
 				g.setColor(Color.black);
@@ -532,6 +657,7 @@ public class Tetris_3 extends Frame {
 			for (int[] coord : nextCoord) {
 				int X = coord[0];
 				int Y = coord[1];
+
 				g.setColor(Color.ORANGE);
 				g.fillRect(X, Y, square, square);
 				g.setColor(Color.black);
@@ -544,6 +670,7 @@ public class Tetris_3 extends Frame {
 			for (int[] coord : nextCoord) {
 				int X = coord[0];
 				int Y = coord[1];
+
 				g.setColor(Color.CYAN);
 				g.fillRect(X, Y, square, square);
 				g.setColor(Color.black);
@@ -759,15 +886,4 @@ public class Tetris_3 extends Frame {
 		}
 	}
 
-}
-
-class Square {
-	int X, Y;
-	Color color;
-
-	Square(int x, int y, Color cl) {
-		X = x;
-		Y = y;
-		color = cl;
-	}
 }
